c498765f3a093a3f94a09bd31f9f68597ebe4754
Shalini <skgshalinigupta@gmail.com>
2023-04-01 14:45:22 +0530

    first commit

---
+++ lib/logic/location.dart
@@ -0,0 +1,15 @@
+import 'package:geolocator/geolocator.dart';
+class Location {
+  double latitude;
+  double longitude;
+  Future<void> getCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition
+        (desiredAccuracy: LocationAccuracy.best);
+      latitude = position.latitude;
+      longitude = position.longitude;
+    } catch (e) {
+      print(e);
+    }
+  }
+}
